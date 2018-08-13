c897583c0b29bdd31a5b614a40e60ad7622ccd63
mab <git@ma-b.com>
2018-08-13 16:22:19 +0200

    Add simple biker icon

--- src/components/emojis.js
+++ src/components/emojis.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
 export const Marcheur = () => <span role="img" aria-label="Marcheur">🚶</span>;
-export const Cycliste = () => <span role="img" aria-label="Vététiste">🚵</span>;
+export const Cycliste = ({ vtt = true }) => (
+  vtt
+    ? <span role="img" aria-label="Vététiste">🚵</span>
+    : <span role="img" aria-label="Cycliste">🚴</span>
+);
 export const Coureur = () => <span role="img" aria-label="Trail">🏃</span>;
